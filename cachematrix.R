1c8e2bee3af86483b4edacca5a28a71470a47f46
Dmitry Golovanov <dgolovan@mail.ru>
2015-05-23 06:04:17 +0300

    PA2 solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Make and use a special "CacheMatrix" as a list of functions 
+## to cache and access the matrix inverse value
 
-## Write a short comment describing this function
+## Create a "CacheMatrix" object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ix <- NULL ## the cached inverse matrix
+  
+  set <- function(y) {
+      x <<- y
+      ix <<- NULL ## clear the cache value
+  }
+  
+  get <- function() x
+  
+  setinv <- function(minv) ix <<- minv
+  getinv <- function() ix
+  
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## Calculate inverse matrix and cache it or return value from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+                                
+  mm <- x$getinv()
+  if (!is.null(mm)) 
+    return(mm) ## get cached inverse matrix
+  
+  data <- x$get() ## use matrix value given
+  mm <- solve(data) ## calculate inverse matrix value
+  x$setinv(mm) ## cache inverse mtrix value
+  mm
 }
